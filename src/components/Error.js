a5e963bdb6195023e7ef4c4d7fa712bab8b16325
fannysofia.takala@gmail.com <fannysofia.takala@gmail.com>
2022-03-16 20:10:24 +0200

    Add Error component to show error message if location is invalid

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+const Error = () => {
+  return (
+    <>
+      <section className="error">
+        <div>Oops! No data available.</div>
+        <div>Please, enter a valid location.</div>
+      </section>
+    </>
+  )
+}
+
+export default Error
