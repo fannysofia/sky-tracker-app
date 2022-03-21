a563d6c77292cee363b34b5ac10ec82369abc323
fannysofia.takala@gmail.com <fannysofia.takala@gmail.com>
2022-03-21 18:49:26 +0200

    Update About page content about problem cases

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div className='about'>
         <h2>About</h2>
-        <p className='version'>This is version 1.0 of Skytracker.</p>
+        <p className='version'>This is version 1.0 of Sky Tracker App.</p>
+        <p>Problems finding the correct location?</p>
+        <p>In that case, define also the country abbreviation. For example: Instead of Melbourne, type Melbourne, AU.</p>
         <Link to="/">Go Back</Link>
     </div>
   )
