e7da3458b552557190606402271721c98f9427fb
fannysofia.takala@gmail.com <fannysofia.takala@gmail.com>
2022-04-13 07:13:12 +0300

    Add unit test todos for Location component

---
+++ src/components/Location.test.js
@@ -0,0 +1,15 @@
+import {render, screen} from '@testing-library/react'
+import Location from './Location'
+
+describe('Location Component', () => {
+     test.todo('keyboard navigation')
+     test.todo('input focus')
+     test.todo('input typing')
+     test.todo('input pasting')
+     test.todo('button focus')
+     test.todo('button click')
+     test.todo('button enter')
+     test.todo('button hover')
+     test.todo('input validation')
+     test.todo('input placeholder text')
+})
