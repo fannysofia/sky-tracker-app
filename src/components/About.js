3ae462e85f4c9fa87dae2270bfb7f78ce089b540
fannysofia.takala@gmail.com <fannysofia.takala@gmail.com>
2022-08-16 20:32:27 +0300

    Direct link to home page
    Direct link to home page to avoid redirecting to 404 page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <p className='version'>This is version 1.0 of Sky Tracker App.</p>
         <p>Problems finding the correct location?</p>
         <p>In that case, define also the country abbreviation. For example: Instead of Melbourne, type Melbourne, AU.</p>
-        <Link to="/">Go Back</Link>
+        <Link to="/sky-tracker-app">Go Back</Link>
     </div>
   )
 }
